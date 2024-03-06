030ed4e795278de9d2669a568522f262a93e692f
p1311836587 <p1311836587@dn.uleam.edu.ec>
2024-03-05 22:44:33 -0500

    api_periodos

---
+++ app/Http/Controllers/CpuPeriodosController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use App\Models\CpuPeriodos;
+use Illuminate\Routing\Controller;
+
+class CpuPeriodosController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function consultarPeriodos()
+    {
+        $periodos = CpuPeriodos::all();
+
+        return response()->json($periodos);
+    }
+
+}

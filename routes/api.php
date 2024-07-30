14c73a67956baa16da693786f14b579b7520f096
stalinvilla <cesarvillavicenciopalacios@gmail.com>
2024-07-30 10:40:35 -0500

    cesar 30-07

--- routes/api.php
+++ routes/api.php
@@ -314,6 +314,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     //guardar atenciones
     Route::post('/atenciones/guardar', [CpuAtencionesController::class, 'guardarAtencion']);
+    Route::get('/atenciones/{id_persona}/{id_funcionario}', [CpuAtencionesController::class, 'obtenerAtencionesPorPaciente']);
+    Route::delete('/atencionesEliminar/{atencionId}', [CpuAtencionesController::class, 'eliminarAtencion']);
 });
 
 // Route::put('/cpu-persona-update/{cedula}', [CpuPersonaController::class, 'update']);

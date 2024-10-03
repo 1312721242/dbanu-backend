8827fb60b2d2db7dffbdf7610845de6773eec0fc
Cesar Stalin <cesarvillavicenciopalacios@gmail.com>
2024-10-03 08:11:01 -0500

    cesar

--- app/Http/Controllers/CpuAtencionOdontologiaController.php
+++ app/Http/Controllers/CpuAtencionOdontologiaController.php
@@ -1,33 +1,42 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\CpuAtencionOdontologia;
 use App\Models\CpuAtencion;
 use App\Models\CpuDiente;
+use App\Models\CpuDerivacion;
+use App\Models\CpuAtencionTriaje;
+use App\Models\CpuTurno;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-
 class CpuAtencionOdontologiaController extends Controller
 {
     public function store(Request $request)
     {
         try {
+            \Log::info('Datos recibidos para atención odontológica', $request->all());
             DB::beginTransaction();
-
             // Validar los datos del request, permitiendo que los campos de diagnóstico y otros sean opcionales
             $validator = Validator::make($request->all(), [
-                'atencion.id_persona' => 'required|exists:cpu_personas,id',
+                'atencion.id_persona' => 'required|integer|exists:cpu_personas,id',
+                'atencion.id_funcionario' => 'required|integer|exists:users,id',
+                'atencion.via_atencion' => 'required|string',
                 'atencion.motivo_atencion' => 'required|string',
+                'atencion.enfermedad_proble_actual' => 'nullable|string',
                 'odontograma' => 'required|array',
-                'odontograma.adulto' => 'required|array',
-                'diagnostico' => 'nullable|array', // Ahora permite que diagnostico sea opcional y de tipo array
-                'examen_estomatognatico' => 'nullable|array', // Permite examen_estomatognatico como opcional
-                'tratamientos' => 'nullable|array|min:0', // Permite tratamientos como opcional y acepta array vacío
-                'planes' => 'nullable|array|min:0', // Permite planes como opcional y acepta array vacío
+                'odontograma.adulto' => 'required|array|min:1',
+                'diagnostico' => 'nullable|array|min:0',
+                'examen_estomatognatico' => 'nullable|array|min:0',
+                'tratamientos' => 'nullable|array|min:0',
+                'planes' => 'nullable|array|min:0',
+                'planes.biometria' => 'nullable|array',
+                'planes.quimica_sanguinea' => 'nullable|array',
+                'planes.rayos_x' => 'nullable|array',
+                'planes.otros' => 'nullable|array',
+            
             ]);
-
+            
+            
             // Validación de fallos
             if ($validator->fails()) {
                 return response()->json([
@@ -67,7 +76,6 @@ class CpuAtencionOdontologiaController extends Controller
                     'arcada' => $arcada, // Guardar la estructura de arcada como un array
                 ]);
             }
-
             // Crear la atención odontológica específica
             $atencionOdontologica = CpuAtencionOdontologia::create([
                 'id_cpu_atencion' => $atencion->id,
@@ -77,7 +85,31 @@ class CpuAtencionOdontologiaController extends Controller
                 'planes' => !empty($request->planes) ? json_encode($request->planes) : null,
                 'tratamiento' => !empty($request->tratamientos) ? json_encode($request->tratamientos) : null,
             ]);
+            // Guardar datos de derivación si el switch de derivación está activo
+            if ($request->input('atencion.derivacionActive')) {
+                $derivacion = $request->input('derivacion');
+              // Validar los datos de la derivación
+    $derivacionData = Validator::make($derivacion, [
+        'id_doctor_al_que_derivan' => 'required|integer|exists:users,id',
+        'id_paciente' => 'required|integer|exists:cpu_personas,id',
+        'motivo_derivacion' => 'required|string',
+        'id_area' => 'required|integer',
+        'fecha_para_atencion' => 'required|date',
+        'hora_para_atencion' => 'required|date_format:H:i:s',
+        'id_estado_derivacion' => 'integer|exists:cpu_estados,id',
+        'id_turno_asignado' => 'required|integer|exists:cpu_turnos,id_turnos',
+    ])->validate();
 
+                $derivacionData['ate_id'] = $cpuAtencion->id;
+                $derivacionData['id_funcionario_que_derivo'] = Auth::id();
+                $derivacionData['fecha_derivacion'] = Carbon::now();
+                $derivacion = CpuDerivacion::create($derivacionData);
+
+                // Actualizar el estado del turno si la derivación es exitosa
+                CpuTurno::where('id_turnos', $derivacionData['id_turno_asignado'])
+                    ->update(['estado' => 7]);
+
+            }
             DB::commit();
             return response()->json(['message' => 'Atención guardada con éxito'], 201);
 
